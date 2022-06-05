0e283c2412614c8a04963185dd853dfe67f4f396
risako <lilisako062397@gmail.com>
2022-06-05 20:52:19 +0900

    feat: add index page

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,10 @@
 <template>
-  <div />
+  <div class="h-screen w-screen flex items-center justify-center">
+    <div>
+      <p class="text-center text-3xl">Build success!🎉🎉🎉</p>
+      <p class="text-center">To navigate to the particular page, please go localhost:3000/[vue file name]</p>
+    </div>
+  </div>
 </template>
 
 <script>
